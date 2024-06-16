efad153b82dece487787130d81683e63aaea9981
LukaDudauri <lukadudauri01@gmail.com>
2024-06-16 19:48:29 +0400

    arrow movement

--- scripts/pixi-spritesheet.js
+++ scripts/pixi-spritesheet.js
@@ -1,15 +1,11 @@
 const app = new PIXI.Application({ width: 960, height: 540});
-
 document.body.appendChild(app.view);
-
 PIXI.Assets.load([
     "../character.json",
     "scene/background.png"
 ]).then(() => {
-    // initialize background image
     const background = PIXI.Sprite.from("scene/background.png");
     app.stage.addChild(background);
-
     const middleground = PIXI.Sprite.from("scene/middleground.png");
     app.stage.addChild(middleground);
     
@@ -18,16 +14,26 @@ PIXI.Assets.load([
     app.stage.scale.y = app.view.height / background.height;
 
     const animations = PIXI.Assets.cache.get('../character.json').data.animations;
-
     const character = PIXI.AnimatedSprite.fromFrames(animations["character/walk"]);
     character.animationSpeed = 1 / 6;
-    character.position.set(150, background.height - 780); // almost bottom-left corner of the canvas
+    character.position.set(150, background.height - 780);
     character.play();
     app.stage.addChild(character)
-    console.log(character);
+    console.log(character.x, character.y);
 
     app.ticker.add(delta => {
         const speed = 6;
         character.x = (character.x + speed * delta + 400) % (background.width + 800) - 400;
+    });
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowUp') {
+            character.y -= 50;
+        }
+    });
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowDown') {
+            character.y += 50;
+        }
     });
 });
